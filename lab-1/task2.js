3fc1c4c7b78a26343f3984f63a09c6e051c2b06e
NikPaukov <sirelfarionik@gmail.com>
2023-05-27 15:02:32 +0300

    aa

--- lab-1/task2.js
+++
@@ -1,9 +0,0 @@
-function isAnagram(string1, string2){
-    string2 = string2.split("").reverse().join("");
-    return string1===string2
-}
-
-
-console.log(isAnagram("hello", "olleh")); // true
-console.log(isAnagram("hello", "olle")); // false
-console.log(isAnagram("hello", "handle")); // false
